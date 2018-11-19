97b52fbbb2ff65f359a542d156fa7db578dbebbc
Fabio Negri <fabionegri1990@gmail.com>
2018-11-19 17:33:25 -0300

    Added copyright header to StickerApplication.java.

--- Android/app/src/main/java/com/example/samplestickerapp/StickerApplication.java
+++ Android/app/src/main/java/com/example/samplestickerapp/StickerApplication.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) WhatsApp Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package com.example.samplestickerapp;
 
 import android.app.Application;
