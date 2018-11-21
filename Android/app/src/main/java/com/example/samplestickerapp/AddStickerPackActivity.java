804c3070137a106c291efd85a26f905c6698723b
Simon Xu <simonxuzhe@whatsapp.com>
2018-11-21 15:11:52 -0800

    Improve the experience when both WhatsApp are installed
    in case one WhatsApp is set as default app, we still would like to surface the other app.

--- Android/app/src/main/java/com/example/samplestickerapp/AddStickerPackActivity.java
+++ Android/app/src/main/java/com/example/samplestickerapp/AddStickerPackActivity.java
@@ -23,18 +23,62 @@ import android.widget.Toast;
 
 public abstract class AddStickerPackActivity extends BaseActivity {
     public static final int ADD_PACK = 200;
+    public static final String TAG = "AddStickerPackActivity";
 
     protected void addStickerPackToWhatsApp(String identifier, String stickerPackName) {
-        Intent intent = new Intent();
-        intent.setAction("com.whatsapp.intent.action.ENABLE_STICKER_PACK");
-        intent.putExtra(StickerPackDetailsActivity.EXTRA_STICKER_PACK_ID, identifier);
-        intent.putExtra(StickerPackDetailsActivity.EXTRA_STICKER_PACK_AUTHORITY, BuildConfig.CONTENT_PROVIDER_AUTHORITY);
-        intent.putExtra(StickerPackDetailsActivity.EXTRA_STICKER_PACK_NAME, stickerPackName);
+        try {
+            //if neither WhatsApp Consumer or WhatsApp Business is installed, then tell user to install the apps.
+            if (!WhitelistCheck.isWhatsAppConsumerAppInstalled(getPackageManager()) && !WhitelistCheck.isWhatsAppSmbAppInstalled(getPackageManager())) {
+                Toast.makeText(this, R.string.error_adding_sticker_pack, Toast.LENGTH_LONG).show();
+                return;
+            }
+            final boolean stickerPackWhitelistedInWhatsAppConsumer = WhitelistCheck.isStickerPackWhitelistedInWhatsAppConsumer(this, identifier);
+            final boolean stickerPackWhitelistedInWhatsAppSmb = WhitelistCheck.isStickerPackWhitelistedInWhatsAppSmb(this, identifier);
+            if (!stickerPackWhitelistedInWhatsAppConsumer && !stickerPackWhitelistedInWhatsAppSmb) {
+                //ask users which app to add the pack to.
+                launchIntentToAddPackToChooser(identifier, stickerPackName);
+            } else if (!stickerPackWhitelistedInWhatsAppConsumer) {
+                launchIntentToAddPackToSpecificPackage(identifier, stickerPackName, WhitelistCheck.CONSUMER_WHATSAPP_PACKAGE_NAME);
+            } else if (!stickerPackWhitelistedInWhatsAppSmb) {
+                launchIntentToAddPackToSpecificPackage(identifier, stickerPackName, WhitelistCheck.SMB_WHATSAPP_PACKAGE_NAME);
+            } else {
+                Toast.makeText(this, R.string.error_adding_sticker_pack, Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "error adding sticker pack to WhatsApp",  e);
+            Toast.makeText(this, R.string.error_adding_sticker_pack, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private void launchIntentToAddPackToSpecificPackage(String identifier, String stickerPackName, String whatsappPackageName) {
+        Intent intent = createIntentToAddStickerPack(identifier, stickerPackName);
+        intent.setPackage(whatsappPackageName);
         try {
             startActivityForResult(intent, ADD_PACK);
         } catch (ActivityNotFoundException e) {
             Toast.makeText(this, R.string.error_adding_sticker_pack, Toast.LENGTH_LONG).show();
         }
+    }
+
+    //Handle cases either of WhatsApp are set as default app to handle this intent. We still want users to see both options.
+    private void launchIntentToAddPackToChooser(String identifier, String stickerPackName) {
+        Intent intent = createIntentToAddStickerPack(identifier, stickerPackName);
+        try {
+            startActivityForResult(Intent.createChooser(intent, getString(R.string.add_to_whatsapp)), ADD_PACK);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, R.string.error_adding_sticker_pack, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @NonNull
+    private Intent createIntentToAddStickerPack(String identifier, String stickerPackName) {
+        Intent intent = new Intent();
+        intent.setAction("com.whatsapp.intent.action.ENABLE_STICKER_PACK");
+        intent.putExtra(StickerPackDetailsActivity.EXTRA_STICKER_PACK_ID, identifier);
+        intent.putExtra(StickerPackDetailsActivity.EXTRA_STICKER_PACK_AUTHORITY, BuildConfig.CONTENT_PROVIDER_AUTHORITY);
+        intent.putExtra(StickerPackDetailsActivity.EXTRA_STICKER_PACK_NAME, stickerPackName);
+        return intent;
     }
 
     @Override
@@ -49,7 +93,7 @@ public abstract class AddStickerPackActivity extends BaseActivity {
                             //validation error should be shown to developer only, not users.
                             MessageDialogFragment.newInstance(R.string.title_validation_error, validationError).show(getSupportFragmentManager(), "validation error");
                         }
-                        Log.e("AddStickerPackActivity", "Validation failed:" + validationError);
+                        Log.e(TAG, "Validation failed:" + validationError);
                     }
                 } else {
                     new StickerPackNotAddedMessageFragment().show(getSupportFragmentManager(), "sticker_pack_not_added");
