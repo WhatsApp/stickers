de2bb06919d4af353f5c6342514a13a0f5568e42
emrcftci <emreciftci541@gmail.com>
2019-12-25 14:05:46 +0300

    refactor(*): Upgrade to Swift 5

--- iOS/WAStickersThirdParty/Interoperability.swift
+++ iOS/WAStickersThirdParty/Interoperability.swift
@@ -37,7 +37,7 @@ struct Interoperability {
         }
 
         if #available(iOS 10.0, *) {
-            pasteboard.setItems([[PasteboardStickerPackDataType: dataToSend]], options: [UIPasteboardOption.localOnly: true, UIPasteboardOption.expirationDate: NSDate(timeIntervalSinceNow: PasteboardExpirationSeconds)])
+            pasteboard.setItems([[PasteboardStickerPackDataType: dataToSend]], options: [UIPasteboard.OptionsKey.localOnly: true, UIPasteboard.OptionsKey.expirationDate: NSDate(timeIntervalSinceNow: PasteboardExpirationSeconds)])
         } else {
             pasteboard.setData(dataToSend, forPasteboardType: PasteboardStickerPackDataType)
         }
