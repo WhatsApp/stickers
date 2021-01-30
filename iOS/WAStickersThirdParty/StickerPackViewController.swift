f7e5cf827df58d4713fe89bd72a3851025e2da24
Sofya Freyman <sfreyman@fb.com>
2021-01-29 18:08:40 -0600

    [iOS] Render animation icon for animated sticker packs

--- iOS/WAStickersThirdParty/StickerPackViewController.swift
+++ iOS/WAStickersThirdParty/StickerPackViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class StickerPackViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
     @IBOutlet private weak var stickerPackPublisherLabel: UILabel!
+    @IBOutlet private weak var stickerPackAnimationIcon: UIImageView!
     @IBOutlet private weak var stickersCollectionView: UICollectionView!
     @IBOutlet private weak var bottomCollectionViewConstraint: NSLayoutConstraint!
 
@@ -82,6 +83,7 @@ class StickerPackViewController: UIViewController, UICollectionViewDataSource, U
         view.addSubview(shareButton)
 
         stickerPackPublisherLabel.text = "\(stickerPack.publisher) • \(stickerPack.formattedSize)"
+        stickerPackAnimationIcon.isHidden = !stickerPack.animated
 
         let tapGuideLabel: UILabel = UILabel()
         tapGuideLabel.text = "Tap on any sticker to copy or share it"
